59ac73049bb0a65c5dd68b3fab29103f337614e8
dkumar1515 <dkumar15@hawk.iit.edu>
2025-05-07 15:24:09 -0500

    first commit

---
+++ user-service/src/main/java/com/example/user_service/UserController.java
@@ -0,0 +1,11 @@
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserController {
+    @GetMapping("/test")
+    public String getUsers() {
+        return "Hello from user-service!";
+    }
+}
